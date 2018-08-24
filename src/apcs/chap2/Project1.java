582d39d7a7863d07f23cb35790669b5bdb97377e
Mason Rein <Mason Rein@DESKTOP-TO951AV.hsd1.co.comcast.net>
2018-08-24 15:55:36 -0600

    Chapter two projects done

--- src/apcs/chap2/Project1.java
+++ src/apcs/chap2/Project1.java
@@ -2,9 +2,9 @@ package apcs.chap2;
 
 public class Project1 {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		// TODO Auto-generated method stub
-
+			System.out.println("Mason\tW 657 SandleWood Ct \t720-600-1125");
 	}
 
 }
--- src/apcs/chap2/Project3.java
+++ src/apcs/chap2/Project3.java
@@ -1,10 +1,19 @@
 package apcs.chap2;
 
+import java.util.Scanner;
+
 public class Project3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner reader = new Scanner (System.in);
+		int x;
+		double y;
+		System.out.println("Enter an kilometer to be converted into Nautical Miles:");
+		x=reader.nextInt();
+		y=x*0.539957;
+		System.out.println(x + " kilometers is: " + y + " Nautical Miles");
+		reader.close();
 	}
 
 }
