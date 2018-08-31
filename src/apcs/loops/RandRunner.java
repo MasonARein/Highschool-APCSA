5ead17452cc36a4520e4726eb32dec493d0b05c9
Mason Rein <Mason Rein@DESKTOP-TO951AV.hsd1.co.comcast.net>
2018-08-30 19:37:03 -0600

    Finished 8/30 work

--- src/apcs/loops/RandRunner.java
+++ src/apcs/loops/RandRunner.java
@@ -6,6 +6,11 @@ public class RandRunner {
 		// TODO Auto-generated method stub
 		int x = 10;
 		double a = 0;
+		int f = 10;
+		int e = 0;
+		int h = 10;
+		int u = 10;
+		int q = 10;
 		System.out.println("1.");
 		while(x >= 1) {
 			double y = Math.random();
@@ -17,7 +22,41 @@ public class RandRunner {
 		while(a <= 0.9) {
 			a = Math.random();
 			System.out.println(a);
-			x--;
+		}
+		System.out.println(" ");
+		System.out.println("3.");
+		while(f > 0) {
+			double d = Math.random();
+			if(d >= 0.5) {
+				e = e + 1;
+			}
+			System.out.println(d);
+			f--;
+		}
+		System.out.println("There were: " + e + " numbers over 0.5");
+		System.out.println(" ");
+		System.out.println("4.");
+		while(h >= 1) {
+			double y = Math.random();
+			y *= 10;
+			System.out.println(y);
+			h--;
+		}
+		System.out.println(" ");
+		System.out.println("5.");
+		while(u >= 1) {
+			double y = Math.random();
+			y = (30 * y) + 70;
+			System.out.println(y);
+			u--;
+		}
+		System.out.println(" ");
+		System.out.println("6.");
+		while(q >= 1) {
+			double y = Math.random();
+			y = (int)((5 * y) + 1);
+			System.out.println(y);
+			q--;
 		}
 	}
 
