a09112dfffa114e375be8458f49e98bf2c74e60a
Mason Rein <Mason Rein@10.15.40.224>
2018-09-04 10:13:52 -0600

    loop runner

--- src/apcs/loops/LoopRunner.java
+++ src/apcs/loops/LoopRunner.java
@@ -4,10 +4,31 @@ public class LoopRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 100;
-		while(x >= 0) {
-			System.out.print(x + ", ");
-			x-=2;
+		int a = 0;
+		int b = 7;
+		int c = 12;
+		int d = 100;
+
+		while(a <= 8) {
+			System.out.print(a + ", ");
+			a++;
+		}
+		
+		System.out.println("");
+
+		while(b <= 12) {
+			System.out.print(b + ", ");
+			b++;
+		}
+		System.out.println("");
+		while(c <= 30) {
+			System.out.print(c + ", ");
+			c+=3;
+		}
+		System.out.println("");
+		while(d >= 0) {
+			System.out.print(d + ", ");
+			d-=2;
 		}
 	}
 
