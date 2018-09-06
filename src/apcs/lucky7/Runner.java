2fcda84b9d08b015e73a6ea4ac1306f1b7ecef18
Mason Rein <Mason Rein@10.15.40.224>
2018-09-05 23:26:42 -0600

    requirements of Lucky 7 completed

--- src/apcs/lucky7/Runner.java
+++ src/apcs/lucky7/Runner.java
@@ -8,23 +8,32 @@ public class Runner {
 		Scanner reader = new Scanner (System.in);
 		int z;
 		int y = 0;
+		int w = 0;
+		int v = 0;
 		System.out.println("How much money do you have?");
 		z=reader.nextInt();
 		
 		for (;z > 0;) {
-			--z;
+			++y;
+			z--;
 			System.out.println("");
 			double x = Math.random();
 			x = (int)((x*10)+2);
-			System.out.println("Dice Roll: " + x);
+			System.out.println("Dice Roll: " + y);
+			System.out.println("Result: " + x);
 			if (x == 7) {
 				z = z + 5;
 			}
+			if (z > w) {
+				w = z;
+				v = y;
+			}
 			System.out.println("Current Amount of Money: " + z);
-			y++;
 		}
 		System.out.println("");
 		System.out.println("Number of Rolls:" + y);
+		System.out.println("The max amount of money was " + w + " at roll number " + v);
+
 		
 	}
 
