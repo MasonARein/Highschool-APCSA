16d8be79b01685bfc76e40403a16c57c86e10596
marein01 <marein01@P2P5DE337.admin.bvsd.k12.co.us>
2018-09-10 14:24:35 -0600

    PiRunner part 2 started

--- src/apcs/pi/PiRunner.java
+++ src/apcs/pi/PiRunner.java
@@ -8,26 +8,61 @@ public class PiRunner {
 		// TODO Auto-generated method stub
 		Scanner reader = new Scanner(System.in);
 		int reps;
+		int print;
 		double pie = 4.0;
 		int evan = 0;
 		double deno = 1.0;
+		int decimal = 0;
 		System.out.println("How many times should the process repeat?");
 		reps = reader.nextInt();
+		System.out.println("Print all terms? (1-Yes 2-No)");
+		print = reader.nextInt();
+		if(reps < 2) {
+			System.out.println("How many times should the process repeat?");
+			reps = reader.nextInt();
+		}
+		
+		System.out.println("");
+		if(print == 1) {
+		System.out.println("Term # \tDenominator \tTerm \t\t\tApproximation");
+		}
 		
 		for(int x = 0; x < reps; x++) {
 			deno = deno + 2;
 			if(evan%2 == 0){
 				pie = pie - 4/deno;
-				System.out.println(pie);
-
+				if(print == 1) {
+				System.out.println(x + "\t" + deno + "\t \t" + 4.0/deno + "\t" + pie);
+				}
 			}
 			else {
 				pie = pie + 4/deno;
-				System.out.println( pie);
+				if(print == 1) {
+				System.out.println(x + "\t" + deno + "\t \t" + 4.0/deno + "\t" + pie);
+				}
 			}
 			evan++;
 		}
 		System.out.println("Pie approxamation " + pie);
+		
+		System.out.println("To how many decimal places should pi go?");
+		decimal = reader.nextInt();
+		if(decimal < 2) {
+			System.out.println("To how many decimal places should pi go?");
+			decimal = reader.nextInt();
+		}
+		deno = 1.0;
+		evan = 0;
+		for(int x = 0; x < reps; x++) {
+			deno = deno + 2;
+			if(evan%2 == 0){
+				pie = pie - 4/deno;
+			}
+			else {
+				pie = pie + 4/deno;
+			}
+			evan++;
+		}
 	}
 
 }
