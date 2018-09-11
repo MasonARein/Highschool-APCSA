13d219cfbe20ce8f5d9ebf689381f33411721116
marein01 <marein01@P2P5DE337.admin.bvsd.k12.co.us>
2018-09-11 15:08:57 -0600

    PiRunner Done

--- src/apcs/pi/PiRunner.java
+++ src/apcs/pi/PiRunner.java
@@ -17,6 +17,7 @@ public class PiRunner {
 		reps = reader.nextInt();
 		System.out.println("Print all terms? (1-Yes 2-No)");
 		print = reader.nextInt();
+
 		if(reps < 2) {
 			System.out.println("How many times should the process repeat?");
 			reps = reader.nextInt();
@@ -45,16 +46,25 @@ public class PiRunner {
 		}
 		System.out.println("Pie approxamation " + pie);
 		
+		System.out.println("");
+		
+		pie = 4.0;
+		evan = 0;
+		deno = 1.0;
+		decimal = 0;
+		long loops = 0;
+		double repeat = 0.0;
 		System.out.println("To how many decimal places should pi go?");
 		decimal = reader.nextInt();
 		if(decimal < 2) {
 			System.out.println("To how many decimal places should pi go?");
 			decimal = reader.nextInt();
 		}
-		deno = 1.0;
-		evan = 0;
-		for(int x = 0; x < reps; x++) {
+		decimal = (int) Math.pow(10, decimal);
+		while((int)(pie * decimal) != (int)(repeat * decimal)){
 			deno = deno + 2;
+			loops += 1;
+			repeat = pie;
 			if(evan%2 == 0){
 				pie = pie - 4/deno;
 			}
@@ -63,6 +73,32 @@ public class PiRunner {
 			}
 			evan++;
 		}
+		System.out.println(repeat);
+		System.out.println("Number of loops through program:" + loops);
+		
+		System.out.println("");
+		System.out.println("Points in a circle to find pi");
+		System.out.println("");
+		
+		int points;
+		System.out.println("How many points should be used to determine pi?");
+		points = reader.nextInt();
+		if(points < 100) {
+			System.out.println("How many points should be used to determine pi?");
+			points = reader.nextInt();
+		}
+		double cirle = 0.0;
+		for(int i =0;i < points;i++) {
+			double x =(Math.random()*2)-1;
+			double y =(Math.random()*2)-1;
+			if((Math.pow(x, 2) + Math.pow(y, 2)) <= 1) {
+				cirle += 1;
+			}
+		}
+		double total = (cirle/points) * 4.0;
+		System.out.println(total);
+		
+		reader.close();
 	}
 
 }
