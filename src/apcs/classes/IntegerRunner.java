76871ade4335f9157c95f594a397d6953875c8ae
marein01 <marein01@P2P5DE337.admin.bvsd.k12.co.us>
2018-09-18 15:10:25 -0600

    IntRunner

---
+++ src/apcs/classes/IntegerRunner.java
@@ -0,0 +1,28 @@
+package apcs.classes;
+
+public class IntegerRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i1;
+		i1 = new Integer (10);
+		int x;
+		x = i1.intValue();
+		System.out.println("i1 =" + x);
+		
+		Integer i2;
+		i2 = new Integer (10);
+
+		String s;
+		s = i2.toString();
+		System.out.println ("i2 = " + s);
+		
+		Integer i3;
+		i3 = new Integer (20);
+
+
+		System.out.println ("i3 = " + i3.toString());
+
+	}
+
+}
