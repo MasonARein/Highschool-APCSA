d0b4637e001410dab8926cbde6720fc3dbd96c7e
marein01 <marein01@P2P5DE337.admin.bvsd.k12.co.us>
2018-09-25 16:00:43 -0600

    Javadoc for fraction class completed

--- src/apcs/fraction/Fraction.java
+++ src/apcs/fraction/Fraction.java
@@ -1,41 +1,136 @@
 package apcs.fraction;
 
+/**
+ * Uses operations on a set of two fractions and also models fractions using numerator and denominator fractions.
+ * 
+ * @author marein01
+ *
+ */
 public class Fraction {
 	private int numerator;
 	private int denominator;
+	/**
+	 * Uses two integer values that together to construct a fraction
+	 * @param n is the value equal to the numerator 
+	 * @param d is the value equal to the denominator 
+	 */
 	public Fraction (int n, int d) {
 	     numerator = n;
 	     denominator = d;
 	}
 
-	public int getNum() {
+	/**
+	 * gets the value of the numerator
+	 * @return the numerator
+	 * 
+	 */
+	public int getNumerator() {
 		return numerator;
 	}
 
-	public void setNum(int num) {
+	/**
+	 * sets the value of numerator
+	 * @param num sets the value of numerator
+	 */
+	public void setNumerator(int num) {
 		this.numerator = num;
 	}
 
-	public int getDeno() {
+	/**
+	 * gets the value of the denominator
+	 * @return the denominator
+	 * 
+	 */
+	public int getDenominator() {
 		return denominator;
 	}
 
-	public void setDeno(int deno) {
+	/**
+	 * sets the value of numerator
+	 * @param deno sets the value of numerator
+	 */
+	public void setDenominator(int deno) {
 		this.denominator = deno;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString () {
 		String str;
-		str = numerator + " / " + denominator;
+		str = numerator + "/" + denominator;
 		return str;
 	}
-	Fraction add (Fraction other) {
-		int y = this.numerator *other.denominator;
-		int x = this.denominator * other.numerator;
-		int z = x + y;
-		other = z /(this.denominator * other.denominator);
-		return other;
+	/**
+	 * Adds together two fraction values into one
+	 * @param input other fraction item
+	 * 
+	 * @return the return value is the fraction of the two fractions being added together
+	 * 
+	 */
+	Fraction add (Fraction input) {
+		int topa = this.numerator *input.denominator;
+		int topb = this.denominator * input.numerator;
+		int top = topa + topb;
+		Fraction total = new Fraction (top, (this.denominator * input.denominator));  
+		return total;
 		
 	}
+	/**
+	 * Subtracts two different fraction values into one
+	 * @param input other fraction item
+	 * @return the return value is the fraction of the two fractions being subtracted together
+	 * 
+	 */
+	Fraction subtract (Fraction input) {
+		int topa = this.numerator *input.denominator;
+		int topb = this.denominator * input.numerator;
+		int top = topa - topb;
+		Fraction total = new Fraction (top, (this.denominator * input.denominator));  
+		return total;
+		
+	}
+	/**
+	 * Multiplies together two fraction values into one
+	 * @param input other fraction item
+	 * 
+	 * @return the return value is the fraction of the two fractions being multiplied together
+	 * 
+	 */
+	Fraction multiply (Fraction input) {
+		int topa = this.numerator *input.numerator;
+		int topb = this.denominator * input.denominator;
+		Fraction total = new Fraction (topa, topb);  
+		return total;
+		
+	}
+	/**
+	 * Divides two different fraction values into one
+	 * @param input other fraction item
+	 * 
+	 * @return the return value is the fraction of the two fractions being divided together
+	 * 
+	 */
+	Fraction divide (Fraction input) {
+		int topa = this.numerator *input.denominator;
+		int topb = this.denominator * input.numerator;
+		Fraction total = new Fraction (topa, topb);  
+		return total;
+		
+	}
+	/**
+	 * Compares two different fraction values to see if they are equal
+	 * @param input other fraction item
+	 * @return true or false on if fraction is equal to other fraction
+	 */
+	public boolean equals (Fraction input) {
+		int crossone = (int)this.numerator * input.denominator;
+		int crosstwo = (int)this.denominator * input.numerator;
+		if(crossone == crosstwo) {
+			return true;
+		}
+		return false;
+	}
+
 }
