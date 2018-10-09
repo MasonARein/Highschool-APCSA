430937be82e263c112ef1fd0caec2fef70794970
marein01 <marein01@P2P5DE337.admin.bvsd.k12.co.us>
2018-10-09 15:53:44 -0600

    Items worked on

--- src/apcs/guess/GuessRunner.java
+++ src/apcs/guess/GuessRunner.java
@@ -1,10 +1,25 @@
 package apcs.guess;
 
+import java.util.Scanner;
+
+
 public class GuessRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Value rand = new Value();
+		int in;
+		int input;
+		Scanner reader = new Scanner(System.in);
+		System.out.println("What number am I thinking of?");
+		in = reader.nextInt();
+		Value guess = new Value (in);
+		try {
+			
+		}
+		catch(Exception e) {
+			
+		}
 	}
 
 }
--- src/apcs/guess/Value.java
+++ src/apcs/guess/Value.java
@@ -1,5 +1,27 @@
 package apcs.guess;
 
 public class Value {
-
+	private int value;
+	
+	public Value(){
+		value = (int)(Math.random() * 100);
+		
+	}
+	public Value(int value){
+		this.value = value;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public String toString(){
+		String str;
+		str = "" + value;
+		return str;
+	}
+	boolean equals(Value instance) {
+		return this.value == instance.value;
+	}
 }
