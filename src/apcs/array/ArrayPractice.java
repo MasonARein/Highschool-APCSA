2ba4aa16da5fbcce3b472aa67410cd657ee2ba62
marein01 <marein01@P2P5DE337.admin.bvsd.k12.co.us>
2018-10-23 15:10:02 -0600

    Array Start

---
+++ src/apcs/array/ArrayPractice.java
@@ -0,0 +1,15 @@
+package apcs.array;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] school = {"Peak", "to", "Peak"};
+		for (int j = 0; j < 10; j++) {
+		for (int i = 0; i < 3; i++) {
+			System.out.print(school[i] + " ");
+		}
+		}
+	}
+
+}
