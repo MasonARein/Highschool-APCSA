4bb2f29bca72ff52db894eda9d03d1c0b0edde76
marein01 <marein01@P2P5DE337.admin.bvsd.k12.co.us>
2018-11-01 15:11:31 -0600

    Up to 7 now

--- src/apcs/doubleList/DoubleList.java
+++ src/apcs/doubleList/DoubleList.java
@@ -16,6 +16,14 @@ public class DoubleList {
 		myList = new double[20];
 		mySize = 0;
 	}
+	/**
+	 * Sets the current available size of the list and the current size of the list being used
+	 * @param val is value taken in to set the size of the list
+	 */
+	public DoubleList(int val) {
+		myList = new double[val];
+		mySize = 0;
+	}
 	
 	/**
 	 * Current effective size
@@ -31,6 +39,13 @@ public class DoubleList {
 	public int maxSize() {
 		return myList.length;
 	}
+	/**
+	 * Adds an item to the end of the DoubleList
+	 * Precondition: the list is not full
+	 * Postcondition: item is added to the end of the list
+	 * @param item the value to be added
+	 * @throws RuntimeException if list is full
+	*/
 	public void add(double newt) {
 		if(size() == maxSize()) {
 			throw new IndexOutOfBoundsException("The DoubleList is full.");
@@ -41,9 +56,86 @@ public class DoubleList {
 		}
 		
 	}
-	
+	/**
+	 * removes the last item added to the list 
+	 */
+	public void remove() {
+		if(mySize == 0) {
+			throw new IndexOutOfBoundsException("The DoubleList is empty.");
+		}
+		--mySize;
+		myList[mySize] = 0.0;
+
+	}
+	/**
+	 * Searches through the entire list to find the first position of a specified item
+	 * @param search is the double/value being searched for in the list
+	 * @return the place in the list in which the value is, and if not in the list it returns -1
+	 */
+	public int indexOf(double search) {
+		for(int i = 0; i < mySize; i++) {
+			if(myList[i] == search) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	/**
+	 * Searches through the entire list to find the last position of a specified item
+	 * @param search is the double/value being searched for in the list
+	 * @return the place in the list in which the value is, and if not in the list it returns -1
+	 */
+	public int lastIndexOf(double search) {
+		for(int i = mySize-1; i > 0; i--) {
+			if(myList[i] == search) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	/**
+	 * Searches through the entire list to find if the specified item is in the list
+	 * @param search is the double/value being searched for in the list
+	 * @return true if the item is in the list and false if not
+	 */
+	public boolean contains(double search) {
+		for(int i = 0; i < mySize; i++) {
+			if(myList[i] == search) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public double sum() {
+		double total = 0.0;
+		for()
+	}
+	/**
+	 * Makes a list that is identical to the list given that can be used to augment a list and keep the original
+	 * @param val is the list going to be copied
+	 */
+	public DoubleList(double[] val) {
+		mySize = 0;
+		myList = new double[(int) (val.length*1.5)];
+		for(double valval: val) {
+			this.add(valval);
+		}
+
+	}
+	/**
+	 * Creates a string of all the different values in the list using the spots that are occupied and returns empty if there are no values
+	 */
 	public String toString() {
-		String str ="";
+		String str = "";
+		if(mySize != 0) {
+		for(int i = 0; i < mySize; i++) {
+			str = str + "[" + i + "]=>" + myList[i] + "; ";
+		}
+		}
+		else {
+			str = "Empty";
+		}
 		return str;
 	}
+	
 }
--- src/apcs/doubleList/ListTester.java
+++ src/apcs/doubleList/ListTester.java
@@ -8,6 +8,24 @@ public class ListTester {
 		DoubleList aList = new DoubleList ();
 		System.out.println ("size: " + aList.size());
 		System.out.println ("max size: " + aList.maxSize());
+		System.out.println ("Testing add and toString");
+		System.out.println ("List is: " + aList);
+		aList.add (12.0);
+		aList.add (-1.0E305);
+		System.out.println ("List is: " + aList);
+		for (int i = 0; i<18; i++) aList.add ((double)i);
+	
+		try {
+		   aList.add (100.0);  // generates an Exception
+		}
+		catch (Exception e) {
+		   System.out.println ("Error: " + e);
+		}
+		System.out.println ("List is: " + aList);
+		DoubleList bList = new DoubleList (35);
+		for (int i = 0; i<27; i++) bList.add ((double)i);
+		bList.remove();
+		System.out.println ("List is: " + bList);
 	}
 
 }
