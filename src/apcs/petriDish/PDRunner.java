c9f1d7285867d93cea438e08d7c18bdf3fa87921
Mason Rein <marein01@bvsd.org>
2019-01-17 15:22:03 -0700

    Slime done

---
+++ src/apcs/petriDish/PDRunner.java
@@ -0,0 +1,18 @@
+package apcs.petriDish;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.grid.BoundedGrid;
+import info.gridworld.grid.Grid;
+
+import java.awt.*;
+
+
+public class PDRunner {
+    public static void main(String[] args) {
+        Grid<Actor> grid = new BoundedGrid<Actor>(20, 20);
+        ActorWorld world = new ActorWorld (grid);
+        world.add(new Slime(Color.GREEN, 10));
+        world.show();
+    }
+}
--- src/apcs/petriDish/Slime.java
+++ src/apcs/petriDish/Slime.java
@@ -2,5 +2,25 @@ package apcs.petriDish;
 
 import info.gridworld.actor.Actor;
 
+import java.awt.*;
+
 public class Slime extends Actor {
+    private int lifeTime;
+    private int redReduce;
+    private int blueReduce;
+    private int greenReduce;
+    public Slime(Color color, int lifeTime){
+        this.lifeTime = lifeTime;
+        this.setColor(color);
+        redReduce = (255 - getColor().getRed())/ lifeTime;
+        blueReduce = (255 - getColor().getBlue())/ lifeTime;
+        greenReduce = (255 - getColor().getGreen())/ lifeTime;
+    }
+    public void act() {
+        lifeTime = lifeTime - 1;
+        if(lifeTime == 0){
+            removeSelfFromGrid();
+        }
+        this.setColor(new Color(this.getColor().getRed()+redReduce,this.getColor().getGreen()+greenReduce,this.getColor().getBlue()+blueReduce));
+    }
 }
