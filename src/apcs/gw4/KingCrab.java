1707efbbe1220a806a1cf8431496e7d37e734c7a
Mason Rein <marein01@bvsd.org>
2019-04-04 14:57:25 -0600

    King Crab Corrected

--- src/apcs/gw4/KingCrab.java
+++ src/apcs/gw4/KingCrab.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class KingCrab extends CrabCritter {
     public void processActors(ArrayList<Actor> actors){
         for (Actor a : actors){
-            if(getGrid().isValid(a.getLocation().getAdjacentLocation(getDirection()))){
+            if(getGrid().get(a.getLocation().getAdjacentLocation(getDirection())) == null){
                 a.moveTo(a.getLocation().getAdjacentLocation(getDirection()));
             }
             else{
